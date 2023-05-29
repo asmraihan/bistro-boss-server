3fb4df689cc4bf4f36081cc38dcf746b9c958bac
asmraihan <asmraihanctg14@gmail.com>
2023-05-29 11:59:05 +0600

    user info in backend

--- index.js
+++ index.js
@@ -34,6 +34,12 @@ async function run() {
     // user collection apis
     app.post('/users', async (req, res) => {
       const user = req.body;
+      const query = {email: user.email}
+      const existingUser = await usersCollection.findOne(query)
+    
+      if(existingUser){
+        return res.send({message: 'User already exists'})
+      }
       const result = await usersCollection.insertOne(user);
       res.send(result);
     })
