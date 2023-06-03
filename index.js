d60e579dd4ed848dec810ce6ec5b2831030a2d3a
asmraihan <asmraihanctg14@gmail.com>
2023-06-03 11:11:58 +0600

    all is well stripe mod80

--- index.js
+++ index.js
@@ -195,10 +195,14 @@ async function run() {
     });
 
     // payment related apis
-    app.post('/payments',verifyJWT, async (req, res) => {
+    app.post('/payments', verifyJWT, async (req, res) => {
       const payment = req.body
-      const result = await paymentCollection.insertOne(payment)
-      res.send(result)
+      const insertResult = await paymentCollection.insertOne(payment)
+
+      const query = { _id: { $in: payment.cartItems.map(id => new ObjectId(id)) } }
+      const deleteResult = await cartCollection.deleteMany(query)
+
+      res.send({ insertResult, deleteResult })
     })
 
     // Send a ping to confirm a successful connection
